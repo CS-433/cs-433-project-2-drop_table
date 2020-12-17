f8ddc78a11e4ead0bbb27f394891c183a608f538
Tanguy Rocher <trocher@MacBook-Air-de-Tanguy.local>
2020-12-17 09:09:56 +0100

    renaming stuff

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # cs-433-project-2-drop_table
 
+# Introduction
+
 The original repositery for the LCD network can be found [here](https://github.com/hkust-vgd/lcd).
 
 It corresponds to the lcd-base source code.
@@ -10,7 +12,29 @@ The bash scripts used to train the networks are in the "run scripts" folder.
 
 To choose which version to train, simply rename the directory containing the source code to lcd and the corresponding training script as "train.py".
 
+# How to train the Network
 
-Applications : 
-	2D-3D Matching:
-		
+Depending on your configuration, you may needs to change the line 12 in the train.py:
+1. If you are using a computer with less ram than the size of your training size :
+    from lcd.dataset import CrossTripletDataset
+2. If you have a lot of ram and are generating your data with regulars pairs of corresponding images/patches :
+   from lcd.dataset_V1 import CrossTripletDataset
+3. If you have a lot of ram are generating your data using LCD author's script as we explained in our report :
+    from lcd.dataset_V1 import CrossTripletDataset
+
+You will also need to specify the path to your data in the config.json.
+You may also change the others parameters according to your needs.
+
+We also provide a run.sh file designed to run the train.py script on EPFL's Izar cluster that run on slurm.
+
+# How to use a pretrained network
+
+Additionnaly to the authors's pretrained network, you can find in the folder logs/ the severals networks we trained as explained in our report. In all our application, the command line argument --logdir allows to choose which network to use.
+
+# How to run the applications
+
+## 2D-2D match
+
+## 2D-3D match
+
+## sparse to dense point cloud
--- lcd/dataset.py
+++ lcd/dataset.py
@@ -7,25 +7,43 @@ import torch.utils.data as data
 
 
 class CrossTripletDataset(data.Dataset):
-    def __init__(self, root, split):
-        self.points = []
-        self.images = []
+    def __init__(self, root, split, cache_size=32):
+        self.views = 2
+        self.split = split
+        self.metadata = {}
+        self.cache = {}
+        self.cache_size = cache_size
         self.flist = os.path.join(root, "*.h5")
         self.flist = sorted(glob.glob(self.flist))
-        print("Loading the dataset into ram")
         for fname in self.flist:
-            print(f'loading file {fname}', flush = True)
-            pts, ims = self._load_data(fname)
-            self.points += list(pts)
-            self.images += list(ims)
-        print(len(self.points), " elements loaded", flush=True)
+            self._add_metadata(fname)
 
     def __len__(self):
-        return len(self.points)
+        return sum([s for _, s in self.metadata.items()])
 
     def __getitem__(self, i):
-        return self.points[i], self.images[i]
+        for fname, size in self.metadata.items():
+            if i < size:
+                break
+            i -= size
+        if fname not in self.cache:
+            self._load_data(fname)
+        points, images = self.cache[fname]
+        return points[i], images[i]
+
+    def _add_metadata(self, fname):
+        with h5py.File(fname, "r") as h5:
+            assert "points" in h5 and "images" in h5
+            assert h5["points"].shape[0] == h5["images"].shape[0]
+            size = h5["points"].shape[0]
+            self.metadata[fname] = size
 
     def _load_data(self, fname):
+        # Remove a random element from cache
+        if len(self.cache) == self.cache_size:
+            key = list(self.cache.keys())[0]
+            self.cache.pop(key)
+
         h5 = h5py.File(fname, "r")
-        return h5["points"][:], h5["images"][:]
+        data = (h5["points"], h5["images"])
+        self.cache[fname] = data
